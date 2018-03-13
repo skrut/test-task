dcf32770b8db662d1c7a8d769a8f9191221d1e1a
NatePage <nathan.page@loyaltycorp.com.au>
2018-03-13 21:33:12 +1100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# LoyaltyCorp Test Task
+
+This project is the code base of the PHP Test Task for the company LoyaltyCorp. It is based on a really simple version
+of [Laravel Lumen](https://lumen.laravel.com).
---
+++ tests/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample(): void
+    {
+        $this->get('/');
+
+        self::assertEquals($this->app->version(), $this->response->getContent());
+    }
+}
